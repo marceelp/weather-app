104f942a12192af9fca41bf147c596f3efce444c
marceelp <marcelpedaa@googlemail.com>
2023-04-03 19:36:20 +0200

    add: useEffect and fetch stuff

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,26 @@ export default function App() {
   const [location, setLocation] = useState("Hamburg");
   const [timeoutID, setTimeoutID] = useState("");
 
-  // useEffect(() => {
-  //   function fetchLocation(location) {
-  //     const api_key = "98262674b9d64b5afd8cc0a789919977";
-  //     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&cnt=8&appid=${api_key}`;
+  useEffect(() => {
+    function fetchLocation(location) {
+      const api_key = "98262674b9d64b5afd8cc0a789919977";
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&cnt=8&appid=${api_key}`;
 
-  //     fetch(url)
-  //       .then((response) => response.json())
-  //       .then((actualData) => {
-  //         setData(actualData);
-  //       });
-  //   }
+      fetch(url)
+        .then((response) => response.json())
+        .then((actualData) => {
+          setData(actualData);
+        });
+    }
 
-  //   const timeout = setTimeout(() => fetchLocation(location), 1000);
+    const timeout = setTimeout(() => fetchLocation(location), 1000);
 
-  //   setTimeoutID(timeout);
+    setTimeoutID(timeout);
 
-  //   return () => {
-  //     clearTimeout(timeoutID);
-  //   };
-  // }, [location]);
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, [location]);
 
   // function handleInput(event) {
   //   setLocation(event.target.value);
