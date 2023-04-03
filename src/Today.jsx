8d37bee5132f255b7719a3bd56895ad5d8d78e1e
marceelp <marcelpedaa@googlemail.com>
2023-04-03 18:59:03 +0200

    add: import statements @mdi

--- src/Today.jsx
+++ src/Today.jsx
@@ -1,5 +1,5 @@
-// import Icon from "@mdi/react";
-// import { mdiWeatherSunsetUp, mdiWeatherSunsetDown } from "@mdi/js";
+import Icon from "@mdi/react";
+import { mdiWeatherSunsetUp, mdiWeatherSunsetDown } from "@mdi/js";
 
 export default function Today() {
   return (
